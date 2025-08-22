fc0f481673672a871154933aa83e4ee8361df3a4
mahdiaslami <mahdyaslami@gmail.com>
2025-08-22 06:29:02 +0330

    feat: add IconChevronDown.vue

---
+++ src/components/icon/IconChevronDown.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor">
+    <path fill-rule="evenodd"
+      d="M12.53 16.28a.75.75 0 01-1.06 0L4.97 10.03a.75.75 0 011.06-1.06l5.97 5.97 5.97-5.97a.75.75 0 111.06 1.06l-5.97 5.97z"
+      clip-rule="evenodd" />
+  </svg>
+</template>
