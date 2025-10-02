1cb8077ec99187b677eac2c8ba4bfd687a2bfdbe
mahdiaslami <mahdyaslami@gmail.com>
2025-10-02 17:11:41 +0330

    feat: add main dashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TaskPage from '@/pages/TaskPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +6,7 @@ const router = createRouter({
     {
       name: 'Home',
       path: '/',
-      component: TaskPage
+      component: () => import('../pages/dashboard/Index.vue')
     }
   ],
 })
