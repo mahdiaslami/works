1546436bd9a7a2fee29356a9f56b0be5c15dd29b
mahdiaslami <mahdyaslami@gmail.com>
2025-10-09 07:13:40 +0330

    feat: add StoryTable

---
+++ src/components/StoryTable.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type Task from '@/app/Task';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+defineProps<{
+  list: Task[]
+}>()
+
+</script>
+
+<template>
+  <Table>
+    <TableCaption>A list of stories.</TableCaption>
+    <TableHeader>
+      <TableRow>
+        <TableHead>
+          Title
+        </TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow v-for="task in list"
+        :key="task.id">
+        <TableCell>
+          {{ task.title }}
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+</template>
