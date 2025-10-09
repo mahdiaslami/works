36da1939fdb3699991dd558f9cc6f20a4c2663f7
mahdiaslami <mahdyaslami@gmail.com>
2025-10-09 07:22:29 +0330

    feat(factories): add task factory

--- src/support/factories.ts
+++ src/support/factories.ts
@@ -2,6 +2,7 @@ import Label from "@/app/Label";
 import Factory from "./Factory";
 import { randomBetween } from "./helper";
 import { faker } from "@faker-js/faker";
+import Task from "@/app/Task";
 
 Factory.define('label', () => {
   return new Label({
@@ -10,5 +11,12 @@ Factory.define('label', () => {
     name: faker.color.human(),
     valueType: randomBetween(['none', 'string', 'number',]),
     valueItems: []
+  })
+})
+
+Factory.define('task', () => {
+  return new Task({
+    id: Date.now(),
+    title: faker.lorem.sentence(5)
   })
 })
