38c6f8341954502f3380a21b6b002f15bed5c80d
mahdiaslami <mahdyaslami@gmail.com>
2025-10-09 12:18:00 +0330

    feat(iteration): add IterationToolbar component

---
+++ src/components/IterationToolbar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+</script>
+
+<template>
+  <div class="flex items-center py-4">
+    <Input class="max-w-sm"
+      placeholder="Filter" />
+
+    <Button class="ms-auto">Create Iteration</Button>
+  </div>
+</template>
