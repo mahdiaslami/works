f4219351dbc8831e15bece526967920cd6d64f87
mahdiaslami <mahdyaslami@gmail.com>
2025-10-09 07:55:03 +0330

    feat(support): add seed function

--- src/app/support/helper.ts
+++ src/app/support/helper.ts
@@ -1,7 +1,9 @@
+import Story from "../Story";
+import StoryRepository from "../StoryRepository";
 import Factory from "./Factory";
 
-export function factory(type: string, count = 1) {
-  return new Factory({ count, type})
+export function factory<T>(type: string, count = 1) {
+  return new Factory<T>({ count, type})
 }
 
 export function randomBetween(arr: any[]): any {
@@ -10,4 +12,11 @@ export function randomBetween(arr: any[]): any {
 
 export function randomInt(max: number, min = 0) {
   return Math.floor(Math.random() * max + min)
+}
+
+export function seed() {
+  const storyRepository = new StoryRepository();
+
+  (<Story[]>factory<Story>('story', 20).create())
+    .forEach((s: Story) => storyRepository.save(s))
 }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import '@/app/support/factories'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { seed } from "./app/support/helper"
+
+(<any>window).app = {
+  seed
+}
 
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
