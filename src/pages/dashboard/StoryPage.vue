94c464e930ee1e7df3edf1dd298d3d1e80ed0c92
mahdiaslami <mahdyaslami@gmail.com>
2025-10-09 07:22:47 +0330

    feat(dashboard): implement story table on StoryPage

--- src/pages/dashboard/StoryPage.vue
+++ src/pages/dashboard/StoryPage.vue
@@ -1,10 +1,13 @@
+<script setup lang="ts">
+import StoryTable from '@/components/StoryTable.vue';
+import { factory } from '@/support/helper';
+
+const stories = factory('task', 100).create()
+
+</script>
+
 <template>
-  <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-    <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-      <div class="aspect-video rounded-xl bg-muted/50" />
-      <div class="aspect-video rounded-xl bg-muted/50" />
-      <div class="aspect-video rounded-xl bg-muted/50" />
-    </div>
-    <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+  <div class="p-4 pt-0">
+    <StoryTable :list="stories" />
   </div>
 </template>
