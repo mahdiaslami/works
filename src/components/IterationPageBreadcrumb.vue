7e596f73f15d81bf53aca48ab200778879dd0319
mahdiaslami <mahdyaslami@gmail.com>
2025-10-09 11:02:34 +0330

    feat(dashboard): add IterationPageBreadcrumb component and IterationPage

---
+++ src/components/IterationPageBreadcrumb.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+</script>
+
+<template>
+  <Breadcrumb>
+    <BreadcrumbList>
+      <BreadcrumbItem class="hidden md:block">
+        <BreadcrumbLink href="/">
+          Home
+        </BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator class="hidden md:block" />
+      <BreadcrumbItem>
+        <BreadcrumbPage>Iterations</BreadcrumbPage>
+      </BreadcrumbItem>
+    </BreadcrumbList>
+  </Breadcrumb>
+</template>
---
+++ src/pages/dashboard/IterationPage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import DashboardHeader from '@/components/DashboardHeader.vue';
+import IterationPageBreadcrumb from '@/components/IterationPageBreadcrumb.vue';
+
+</script>
+
+<template>
+  <DashboardHeader>
+    <IterationPageBreadcrumb />
+  </DashboardHeader>
+  <div class="p-4 pt-0">
+  </div>
+</template>
