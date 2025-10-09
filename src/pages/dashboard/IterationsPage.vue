cbf0d48e5636619c430656a6170373ebc8e57f3d
mahdiaslami <mahdyaslami@gmail.com>
2025-10-09 12:18:23 +0330

    feat(dashboard): integrate IterationToolbar into IterationsPage

--- src/pages/dashboard/IterationsPage.vue
+++ src/pages/dashboard/IterationsPage.vue
@@ -3,6 +3,7 @@ import { iterationService } from '@/app';
 import DashboardHeader from '@/components/DashboardHeader.vue';
 import IterationsPageBreadcrumb from '@/components/IterationsPageBreadcrumb.vue';
 import IterationTable from '@/components/IterationTable.vue';
+import IterationToolbar from '@/components/IterationToolbar.vue';
 import TableContainer from '@/components/TableContainer.vue';
 
 const iterations = iterationService.all()
@@ -12,6 +13,10 @@ const iterations = iterationService.all()
   <DashboardHeader>
     <IterationsPageBreadcrumb />
   </DashboardHeader>
+
+  <div class="w-full px-4">
+    <IterationToolbar />
+  </div>
 
   <div class="px-4 pb-4">
     <TableContainer>
