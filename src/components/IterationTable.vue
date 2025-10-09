fd598d358117e6a687f23b5defae4b15c0251c18
mahdiaslami <mahdyaslami@gmail.com>
2025-10-09 11:54:12 +0330

    feat(dashboard): add IterationTable component

---
+++ src/components/IterationTable.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import type Iteration from '@/app/Iteration';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+defineProps<{
+  list: Iteration[]
+}>()
+
+</script>
+
+<template>
+  <Table>
+    <TableCaption>A list of stories.</TableCaption>
+    <TableHeader>
+      <TableRow>
+        <TableHead>
+          ID
+        </TableHead>
+        <TableHead>
+          Start Date
+        </TableHead>
+        <TableHead>
+          End Date
+        </TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow v-for="iteration in list"
+        :key="iteration.id">
+        <TableCell>
+          {{ iteration.id }}
+        </TableCell>
+        <TableCell>
+          {{ iteration.startDate.toLocalDateString() }}
+        </TableCell>
+        <TableCell>
+          {{ iteration.endDate.toLocalDateString() }}
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+</template>
--- src/pages/dashboard/IterationsPage.vue
+++ src/pages/dashboard/IterationsPage.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import { iterationService } from '@/app';
 import DashboardHeader from '@/components/DashboardHeader.vue';
 import IterationsPageBreadcrumb from '@/components/IterationsPageBreadcrumb.vue';
+import IterationTable from '@/components/IterationTable.vue';
 
+const iterations = iterationService.all()
 </script>
 
 <template>
@@ -9,5 +12,6 @@ import IterationsPageBreadcrumb from '@/components/IterationsPageBreadcrumb.vue'
     <IterationsPageBreadcrumb />
   </DashboardHeader>
   <div class="p-4 pt-0">
+    <IterationTable :list="iterations" />
   </div>
 </template>
