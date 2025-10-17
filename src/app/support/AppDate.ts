0ba0aa518430365002ee275a7c343e14023b5eab
mahdiaslami <mahdyaslami@gmail.com>
2025-10-17 13:08:14 +0330

    feat(support): allow different date inputs in AppDate constructor

--- src/app/support/AppDate.ts
+++ src/app/support/AppDate.ts
@@ -1,7 +1,7 @@
 import PersianDate from "./PersianDate";
 
 export default class AppDate extends PersianDate{
-  constructor(date: Date = new Date) {
+  constructor(date: number | string | Date = new Date) {
     super(date)
     this._date.setHours(0, 0, 0, 0);
   }
