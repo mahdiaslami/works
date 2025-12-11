5042d6e93af1848a4b65dc0fcc857b49496011c5
mahdiaslami <mahdyaslami@gmail.com>
2025-12-11 19:12:46 +0330

    feat(domain): add fake work repository

--- src/domain/container.ts
+++ src/domain/container.ts
@@ -1,8 +1,9 @@
-
 import { Container } from "@halliganjs/service-container";
 import { WorkRepository } from "./works/work-repository";
 import { WorkService } from "./works/work-service";
 import { GitLab } from "./support/gitlab";
+import { WorkRepository as FakeWorkRepository } from "./fakes/work-repository";
+
 
 export const container = new Container
 
@@ -11,5 +12,14 @@ container.binding(
   () => new GitLab(import.meta.env.VITE_GITLAB_BASE_URL, import.meta.env.VITE_GITLAB_TOKEN)
 )
 
-container.binding(WorkRepository, (c) => new WorkRepository(c.make(GitLab)))
+container.binding(WorkRepository, (c) => {
+  if (isTrue(import.meta.env.VITE_FAKE)) {
+    return new FakeWorkRepository();
+  }
+  return new WorkRepository(c.make(GitLab));
+})
 container.binding(WorkService, (c) => new WorkService(c.make(WorkRepository)))
+
+function isTrue(env: any) {
+  return env === 'true' || env === true
+}
---
+++ src/domain/fakes/work-repository.ts
@@ -0,0 +1,74 @@
+import type { WorkRepository as IWorkRepository } from '../contracts/work-repository';
+import { Work } from '../works/work';
+import { WorkCollection } from '../works/work-collection';
+import { fakeUser } from '../support/factory';
+
+export class WorkRepository implements IWorkRepository {
+  constructor() {}
+
+  async issuesCreatedByMe(): Promise<WorkCollection> {
+    const me = {
+      id: 1,
+      username: 'mahdi.aslami',
+      name: 'Mahdi Aslami',
+      avatarUrl: '',
+      webUrl: ''
+    } as unknown as any;
+
+    const other = fakeUser();
+
+    const created1 = {
+      id: 101,
+      iid: 1001,
+      title: 'Fake issue created by me (assigned to me)',
+      author: me,
+      assignee: me,
+      webUrl: 'https://gitlab.example.com/project/1/-/issues/1001'
+    };
+
+    const created2 = {
+      id: 102,
+      iid: 1002,
+      title: 'Fake issue created by me (assigned to someone else)',
+      author: me,
+      assignee: other,
+      webUrl: 'https://gitlab.example.com/project/1/-/issues/1002'
+    };
+
+    const items = [new Work(created1), new Work(created2)];
+    return new WorkCollection(items);
+  }
+
+  async issuesAssignedToMe(): Promise<WorkCollection> {
+    const me = {
+      id: 1,
+      username: 'mahdi.aslami',
+      name: 'Mahdi Aslami',
+      avatarUrl: '',
+      webUrl: ''
+    } as unknown as any;
+
+    const other = fakeUser();
+
+    const assigned1 = {
+      id: 101,
+      iid: 1001,
+      title: 'Fake issue created by me (assigned to me)',
+      author: me,
+      assignee: me,
+      webUrl: 'https://gitlab.example.com/project/1/-/issues/1001'
+    };
+
+    const assigned2 = {
+      id: 202,
+      iid: 2002,
+      title: 'Fake issue assigned to me (created by someone else)',
+      author: other,
+      assignee: me,
+      webUrl: 'https://gitlab.example.com/project/1/-/issues/2002'
+    };
+
+    const items = [new Work(assigned1), new Work(assigned2)];
+    return new WorkCollection(items);
+  }
+}
