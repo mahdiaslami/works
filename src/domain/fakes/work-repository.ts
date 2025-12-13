8bc78943ecba5e1fdd05a8bcf59ae5334829bc80
mahdiaslami <mahdyaslami@gmail.com>
2025-12-13 18:28:31 +0330

    refactor(fakes): restructure fake repository data

--- src/domain/fakes/work-repository.ts
+++ src/domain/fakes/work-repository.ts
@@ -1,95 +1,50 @@
 import type { WorkRepository as IWorkRepository } from '../contracts/work-repository';
 import { Work } from '../works/work';
 import { WorkCollection } from '../works/work-collection';
-import { fakeUser } from '../support/factory';
+import { fakeWork } from '../support/factory';
+import type { User } from '@/types/gitlab';
+import type { Issue } from '../support/gitlab';
 
 export class WorkRepository implements IWorkRepository {
-  constructor() {}
+  constructor() { }
 
   async issuesCreatedByMe(): Promise<WorkCollection> {
-    const me = {
-      id: 1,
-      username: 'mahdi.aslami',
-      name: 'Mahdi Aslami',
-      avatarUrl: '',
-      webUrl: ''
-    } as unknown as any;
+    const items = issues.map((i) => new Work(i))
+      .filter((w) => w.author === me);
 
-    const other = fakeUser();
-
-    const created1 = {
-      id: 101,
-      iid: 1001,
-      title: 'Fake issue created by me (assigned to me)',
-      author: me,
-      assignee: me,
-      description: `
-[task]
-
-- https://gitlab.example.com/project/1/-/issues/1002
-- https://gitlab.example.com/project/1/-/issues/2002
-
-[/task]
-`,
-      webUrl: 'https://gitlab.example.com/project/1/-/issues/1001'
-    };
-
-    const created2 = {
-      id: 102,
-      iid: 1002,
-      title: 'Fake issue created by me (assigned to someone else)',
-      author: me,
-      assignee: other,
-      description: '',
-      webUrl: 'https://gitlab.example.com/project/1/-/issues/1002'
-    };
-
-    const created3 = {
-      id: 102,
-      iid: 1002,
-      title: 'Fake issue created by me (without assignee)',
-      author: me,
-      assignee: null,
-      description: '',
-      webUrl: 'https://gitlab.example.com/project/1/-/issues/1003'
-    };
-
-    const items = [new Work(created1), new Work(created2), new Work(created3)];
     return new WorkCollection(items);
   }
 
   async issuesAssignedToMe(): Promise<WorkCollection> {
-    const me = {
-      id: 1,
-      username: 'mahdi.aslami',
-      name: 'Mahdi Aslami',
-      avatarUrl: '',
-      webUrl: ''
-    } as unknown as any;
+    const items = issues.map((i) => new Work(i))
+      .filter((w) => w.assignee === me);
 
-    const other = fakeUser();
-
-    const assigned1 = {
-      id: 101,
-      iid: 1001,
-      title: 'Fake issue created by me (assigned to me)',
-      author: me,
-      assignee: me,
-      description: '',
-      webUrl: 'https://gitlab.example.com/project/1/-/issues/1001'
-    };
-
-    const assigned2 = {
-      id: 202,
-      iid: 2002,
-      title: 'Fake issue assigned to me (created by someone else)',
-      author: other,
-      assignee: me,
-      description: '',
-      webUrl: 'https://gitlab.example.com/project/1/-/issues/2002'
-    };
-
-    const items = [new Work(assigned1), new Work(assigned2)];
     return new WorkCollection(items);
   }
 }
+
+// --- Data ---
+
+const me = {
+  id: 1,
+  username: 'mahdi.aslami',
+  name: 'Mahdi Aslami',
+  avatarUrl: '',
+  webUrl: ''
+} as User;
+
+const issues: Issue[] = [
+  { ...fakeWork(), author: me },
+  { ...fakeWork(), author: me, assignee: me },
+  { ...fakeWork(), assignee: me },
+  { ...fakeWork(), assignee: me },
+];
+
+issues[0]!.description = `
+[task]
+
+- ${issues[1]!.webUrl}
+- ${issues[2]!.webUrl}
+
+[/task]
+`
