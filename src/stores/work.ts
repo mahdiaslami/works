50c44132c951f72aa42ab1354c7f8a2bc8da1792
mahdiaslami <mahdyaslami@gmail.com>
2025-12-25 16:04:32 +0330

    refactor(work): remove unused works state

--- src/stores/work.ts
+++ src/stores/work.ts
@@ -1,16 +1,13 @@
 import { ref, type Ref } from 'vue'
 import { defineStore } from 'pinia'
 import { container, WorkService, type Work } from '@/domain'
-import { WorkCollection } from '@/domain/works/work-collection'
 import { Category } from '@/domain/categories/category'
 
 export const useWorkStore = defineStore('work', () => {
   const workService = container.make<WorkService>(WorkService)
-  const works: Ref<WorkCollection> = ref<WorkCollection>(new WorkCollection())
   const categories: Ref<Category[]> = ref<Category[]>([])
 
-  workService.works().then((v) => works.value = v)
   workService.categories().then((c) => categories.value = c)
 
-  return { works, categories }
+  return { categories }
 })
