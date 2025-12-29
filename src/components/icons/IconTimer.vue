e29698dcc631e42a9394fe1c4ed3ffb1ceb0749e
mahdiaslami <mahdyaslami@gmail.com>
2025-12-29 06:12:33 +0330

    feat(ui): add icon components IconClock and IconTimer

---
+++ src/components/icons/IconTimer.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    class="size-6">
+
+    <!-- top button -->
+    <path stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M10 2h4" />
+
+    <!-- stem -->
+    <path stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M12 2v2" />
+
+    <!-- clock body -->
+    <path stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M21 14a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+
+    <!-- hands -->
+    <path stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M12 10v4l3 2" />
+  </svg>
+</template>
